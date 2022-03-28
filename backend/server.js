f9be22eba923561abb72ce3843a91843f36199bc
Michal <michalpaczko@gmail.com>
2022-03-28 20:58:14 +0200

    server base confing

---
+++ backend/server.js
@@ -0,0 +1,18 @@
+// importing
+import express from 'express'
+
+// app config
+const app = express();
+const port = process.env.PORT || 9000;
+
+// middleware
+
+// DB config
+
+// ???
+
+// api routes
+app.get('/',(req,res) => res.status(200).send('hello world'));
+
+// listener
+app.listen(port, () => console.log(`Listening on locaclhost:${port}`));
